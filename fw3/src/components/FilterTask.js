a424c376dc676f0611978fd40cc051a6cb68775b
JurbaDaria <126083934+JurbaDaria@users.noreply.github.com>
2023-12-03 13:06:33 +0200

    My first commit

---
+++ fw3/src/components/FilterTask.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FilterTasks = ({ filterTasks }) => {
+  const handleFilterChange = (e) => {
+    // Логика фильтрации задач
+    const selectedFilter = e.target.value;
+    filterTasks(selectedFilter);
+  };
+
+  return (
+    <select onChange={handleFilterChange}>
+      <option value="all">Все задачи</option>
+      <option value="inProgress">В процессе</option>
+      <option value="completed">Завершенные</option>
+    </select>
+  );
+};
+
+export default FilterTasks;
